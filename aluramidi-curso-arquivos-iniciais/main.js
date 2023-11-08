c5cec40f423d035767d3b1638f8558385966d802
luizsavedra <137305888+luizsavedra@users.noreply.github.com>
2023-11-08 10:46:34 -0300

    D

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,2 +1,28 @@
-alert('Não tem som :(');
-document.querySelector('.tecla_pom')
+function tocaSom(seletorAudio){  
+    const elemento = document.querySelector(seletorAudio).play('');
+    }       
+    
+    
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        alert('Elemento não encontrado');
+    }
+    const lista = document.querySelectorAll('.tecla')    
+    
+    for (contador = 0; contador < lista.legnth;){
+        lista [contador].classList[1];
+    
+        const instrumento = lista[contador];
+        const tecla = lista[contador];
+        const idAudio = `#som_${instrumento}`;
+    
+        tecla[contador].onclick = function(){
+            tocaSom(idAudio);
+        };
+    
+        tecla.onkeydown = function(evento){
+            if(evento.code=='Space' || evento.code ==='Enter'){
+            tecla.classList.add('ativa');}
+        }
+        }
